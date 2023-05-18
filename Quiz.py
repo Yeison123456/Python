655aa063e1fbddc3b1ae664cc4472cc979f92d16
YDRR123 <125160236+Yeison123456@users.noreply.github.com>
2023-05-18 15:51:26 -0500

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,86 @@
+"""Una persona desea iniciar un negocio, para lo cual piensa verificar cuánto dinero le
+prestara el banco por hipotecar su casa. Tiene una cuenta bancaria, pero no quiere
+disponer de ella a menos que el monto por hipotecar su casa sea muy pequeño.
+a. Si el monto de la hipoteca es menor que $1.000.000, entonces invertirá el 50% de
+la inversión total y un socio invertirá el otro 50%.
+b. Si el monto de la hipoteca es de $ 1.000.000 o más, entonces invertirá el monto
+total de la hipoteca y el resto del dinero que se necesite para cubrir la inversión
+total se repartirá a partes iguales entre el socio y el."""
+
+inv=int(input("Cuanto es la inversion de la casa"))
+hip=int(input("Cuanto es la hipoteca de la casa"))
+
+if hip<1000000:
+    pa=int(inv*0.5)
+    print(f"entonces invertirá el 50% ({pa}) de la inversión total y un socio invertirá el otro 50% ({pa}).")
+elif hip>=1000000:
+    to=int(inv-hip)
+    to1=int(to*0.5)
+    to2=int(hip+to1)
+    print(f"entonces invertirá el monto de la hipoteca y mas la mitad de el resto del dinero ({to2}) y la otra mitad la cubrira el socio ({to1})")
+    
+"""El gobierno de Colombia, desea reforestar un bosque que mide determinado número de
+hectáreas. Si la superficie del terreno excede a 1 millón de metros cuadrados, entonces
+decidirá sembrar de la siguiente manera:"""
+
+sup=int(input("Cuantas hectarias desean reforestar"))
+supt=int(sup*10000)
+
+
+
+if supt>1000000:
+    pi=int(supt*0.7)
+    oy=int(sup*0.2)
+    ce=int(sup*0.1)
+    print(f"Sombraran un 70% del terrero ({pi}) en arboles de tipo pinos, un 20% del tenerreno ({oy}) en  arboles de tipo oyamel y un 10% del terreno ({ce}) en arboles de tipo cedro ")
+elif supt<=1000000:
+    pi=int(supt*0.5)
+    oy=int(sup*0.3)
+    ce=int(sup*0.2)
+    print(f"Sombraran un 50% del terrero ({pi}) en arboles de tipo pinos, un 30% del tenerreno ({oy}) en  arboles de tipo oyamel y un 20% del terreno ({ce}) en arboles de tipo cedro ")
+
+
+"""Una persona debe realizar un muestreo con 50 personas para determinar el promedio de
+peso de los niños, jóvenes, adultos y viejos que existen en su zona habitacional. Se
+determinan las categorías con base en la siguiente tabla:"""
+
+cn=1
+cj=1
+ca=1
+cv=1
+n=0
+j=0
+a=0
+v=0
+pn=0
+pj=0
+pa=0
+pv=0
+
+for p in range(10):
+    e=int(input("Cuantos años tiene"))
+    if e>=0 and e<=12:
+        p=int(input("Cuanto pesa el niño?"))
+        n=n+p
+        cn+=1
+    elif e>=13 and e<=29:
+        p=int(input("Cuanto pesa el joven?"))
+        j=j+p
+        cj+=1
+    elif e>=30 and  e<=59:
+        p=int(input("Cuanto pesa el adulto?"))
+        a=a+p
+        ca+=1
+    elif e>=60:
+        p=int(input("Cuanto pesa el viejo?"))
+        v=v+p
+        cv+=1
+pn=n/cn
+pj=j/cj
+pa=a/ca
+pv=v/cv
+
+print(f"El promedio del peso en niños es de: {pn}")
+print(f"El promedio del peso en jovenes es de: {pj}")
+print(f"El promedio del peso en adultos es de: {pa}")
+print(f"El promedio del peso en viejos es de: {pv}")
